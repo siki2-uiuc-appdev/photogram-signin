b07d3578879247811c63f2860394bc5db721a644
siki2-uiuc <siki2@illinois.edu>
2022-10-24 16:20:05 +0000

    User sign up functionality complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,10 +20,16 @@ class UsersController < ApplicationController
     user = User.new
 
     user.username = params.fetch("input_username")
+    user.password= params.fetch("input_password")
+    user.password_confirmation = params.fetch("input_password_confirmation")
 
-    user.save
+    save_status = user.save
 
-    redirect_to("/users/#{user.username}")
+    if save_status
+      redirect_to("/users/#{user.username}", { :notice => "Welcome, #{user.username}!"})
+    else
+      redirect_to("/user_sign_up", { :alert => user.errors.full_messages.to_sentence})
+    end
   end
 
   def update
